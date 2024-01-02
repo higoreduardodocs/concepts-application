e3d9f5784cc31152c2e78aa353fcd9c3c9fe85f5
higoreduardodocs <higoreduardodocs@gmail.com>
2024-01-02 19:21:30 -0400

    :tada: Feat: Create project

---
+++ web/script.js
@@ -0,0 +1,20 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefer-reduced-motion:reduce)").matches) {
+  addAnimated();
+}
+
+function addAnimated() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
